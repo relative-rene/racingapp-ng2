d2aab2838f7c585427d9856e28f5073d04e19bb1
relative-rene <rene.arellano415@gmail.com>
2016-07-06 13:59:38 -0700

    continued refactor mock.ts,html &  css for races

---
+++ app/mock.ts
@@ -0,0 +1,23 @@
+import { Race } from './race';
+export const RACES: race[] = [{
+    "id": 1,
+    "name": "Daytona Thunderdome",
+    "date": new Date('2512-01-04T14:00:00'),
+    "about": "Pace yourself in the Theme park capital of the World.",
+    "entryFee": 3200,
+    "isRacing": false
+  }, {
+    "id": 2,
+    "name": "San Francisco Ruins",
+    "date": new Date('2512-07-03T20:00:00'),
+    "about": "With great hills comes great scenic routes.",
+    "entryFee": 4700,
+    "isRacing": true
+  }, {
+    "id": 3,
+    "name": "New York City Skyline",
+    "date": new Date('2512-07-12T21:00:00'),
+    "about": "If you can make it hear, you can run the concrete jungle.",
+    "entryFee": 4300,
+    "isRacing": true
+  }];
--- app/races.component.ts
+++ app/races.component.ts
@@ -1,47 +1,20 @@
 import { Component } from '@angular/core';
+import { Race } from './race';
+import { RACES } from './mocks';
 
 @Component({
   selector: 'my-races',
-  template: `
-    <h2>Cash left to enter races {{cashLeft() | currency:'USD':true}}</h2>
-    <ul>
-      <li *ngFor="let race of races">
-        <h2>{{race.name}} {{race.entryFee | currency:'USD':true}}</h2>
-        <p>{{race.date | date:'MMM d, y, h:MM a'}}</p>
-        <p>{{race.about}}</p>
-        <button *ngIf="!race.isRacing">Enter Race</button>
-        <h3 *ngIf="race.isRacing">Already Racing</h3>
-      </li>
-    </ul>
-    <h2>Total cost: {{totalCost() | currency:'USD':true}}</h2>
-  `
+  templateUrl: 'app/races.component.html',
+  styleUrls:['app/races.component.css']
+
 })
 export class RacesComponent {
   heading = "Marathon Schedule"
   cash = 10000;
-  races = [{
-    "id": 1,
-    "name": "Daytona Thunderdome",
-    "date": new Date('2512-01-04T14:00:00'),
-    "about": "Pace yourself in the Theme park capital of the World.",
-    "entryFee": 3200,
-    "isRacing": false
-  }, {
-    "id": 2,
-    "name": "San Francisco Ruins",
-    "date": new Date('2512-07-03T20:00:00'),
-    "about": "With great hills comes great scenic routes.",
-    "entryFee": 4700,
-    "isRacing": true
-  }, {
-    "id": 3,
-    "name": "New York City Skyline",
-    "date": new Date('2512-07-12T21:00:00'),
-    "about": "If you can make it hear, you can run the concrete jungle.",
-    "entryFee": 4300,
-    "isRacing": true
-  }];
-
+  races: Race[];
+  ngOnInit() {
+    this.races = RACES
+  }
   totalCost() {
     let sum = 0;
     for (let race of this.races) {
@@ -53,7 +26,4 @@ export class RacesComponent {
   cashLeft() {
     return this.cash - this.totalCost();
   }
-
 }
-`
-})
