0158ce9c93de1f09df016a9f76696e57f8d7d5ed
relative-rene <rene.arellano415@gmail.com>
2016-07-06 16:51:27 -0700

    http routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RacesComponent } from './races.component';
+import { RaceService } from './race.service';
+import { HTTP_PROVIDERS } from '@angular/http';
 
 @Component({
   selector: 'racing-app',
   template: `
-    <h1>{{heading}}</h1>
-    <my-races></my-races>  `,
-     directives: [RacesComponent]
+    <header class="container">
+      <h1>{{heading}}</h1>
+    </header>
+    <my-races></my-races>
+  `,
+  directives: [RacesComponent],
+  providers:[RaceService, HTTP_PROVIDERS]
 })
-
 export class AppComponent {
   heading = "Marathon Schedule"
 }
--- app/races.component.ts
+++ app/races.component.ts
@@ -1,31 +1,42 @@
 import { Component } from '@angular/core';
 import { Race } from './race';
-import { RACES } from './mock';
+import { RaceService } from './race.service';
 
 @Component({
   selector: 'my-races',
   templateUrl: 'app/races.component.html',
   styleUrls:['app/races.component.css']
-
 })
 export class RacesComponent {
-  heading = "Marathon Schedule"
+  heading = "Ultra Racing Schedule"
   cash = 10000;
   races: Race[];
+
+  constructor(private raceService: RaceService) { }
+
   ngOnInit() {
-    this.races = RACES
+    this.raceService.getRaces()
+        .subscribe(data => this.races = data);
   }
+
   totalCost() {
     let sum = 0;
-    for (let race of this.races) {
-      if (race.isRacing) sum += race.entryFee;
+    if (this.races) {
+      for (let race of this.races) {
+        if (race.isRacing) sum += race.entryFee;
+      }
     }
     return sum;
+  }
+
+  castDate(date) {
+    return new Date(date);
   }
 
   cashLeft() {
     return this.cash - this.totalCost();
   }
+
   enterRace(race) {
     if (this.cashLeft() > race.entryFee) {
       race.isRacing = true;
