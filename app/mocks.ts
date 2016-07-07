9ee9604cce0764e94a3d60459b93ff97ca3464e7
relative-rene <rene.arellano415@gmail.com>
2016-07-06 22:42:59 -0700

    boston mock with images

--- app/mocks.ts
+++ app/mocks.ts
@@ -2,23 +2,38 @@ import { Race } from './race';
 
 export const RACES: Race[] = [{
     "id": 1,
-    "name": "Daytona Thunderdome",
-    "date": new Date('2512-01-04T14:00:00'),
-    "about": "Pace yourself in the Theme park capital of the World.",
-    "entryFee": 3200,
-    "isRacing": false
+    "name": "Miami Marathon",
+    "date": new Date('2017-01-04T14:00:00'),
+    "about": "Benvenidos a Miami, the Theme park capital of the World.",
+    "entryFee": 170,
+    "isRacing": false,
+    "image": "/images/miamiMarathon.jpg",
+    "imageDescription": "Miami Marathon route"
   }, {
     "id": 2,
-    "name": "San Francisco Ruins",
-    "date": new Date('2512-07-03T20:00:00'),
+    "name": "San Francisco Marathon",
+    "date": new Date('2017-08-03T20:00:00'),
     "about": "With great hills comes great scenic routes.",
-    "entryFee": 4700,
-    "isRacing": true
+    "entryFee": 115,
+    "isRacing": true,
+    "image": "/images/sfMarathon.jpg",
+    "imageDescription": "SF Marathon route"
   }, {
     "id": 3,
-    "name": "New York City Skyline",
-    "date": new Date('2512-07-12T21:00:00'),
-    "about": "If you can make it hear, you can run the concrete jungle.",
-    "entryFee": 4300,
-    "isRacing": true
+    "name": "New York City Marathon",
+    "date": new Date('2018-11-12T21:00:00'),
+    "about": "If you can make it here, you can run the concrete jungle.",
+    "entryFee": 266,
+    "isRacing": true,
+    "image": "/images/nyMarathon.jpg",
+    "imageDescription": "NY Marathon route"
+  }, {
+    "id":4,
+    "name": "Boston Marathon",
+    "date": new Date('2017-06-12T21:00:00'),
+    "about": "The culminating event in distance running.",
+    "entryFee": 180,
+    "isRacing": false
+    "image": "/images/bostonMarathon.jpg",
+    "imageDescription": "Boston Marathon route"
   }];
