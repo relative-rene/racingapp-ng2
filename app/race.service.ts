13070574dcc924e742ba31eb554bbc27796c3512
relative-rene <rene.arellano415@gmail.com>
2016-07-07 12:32:21 -0700

    constructor built http.get json

--- app/race.service.ts
+++ app/race.service.ts
@@ -9,7 +9,6 @@ export class RaceService {
   constructor(private http: Http) { }
 
   getRaces() {
-    return this.http.get('app/races.json')
-          .map(response => <Race[]>response.json().racesData);
+    return this.http.get('app/races.json').map(response => <Race[]>response.json().racesData);
   }
 }
